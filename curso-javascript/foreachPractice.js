ae59499921b62875d2bd8f7edc8e9a55931e7800
Diego <dabcpop@gmail.com>
2024-05-16 23:41:35 -0500

    added project

--- curso-javascript/foreachPractice.js
+++ curso-javascript/foreachPractice.js
@@ -32,3 +32,21 @@ console.log(isInRaffle)
 console.log(`Name: ${isInRaffle["name"]}
 Direction: ${isInRaffle.direction}
 `)
+// EJERCICIOS
+const transactions=[{id:1,description:'Grocery Shopping',amount:-50},{id:2,description:'Salary Deposit',amount:2000},{id:3,description:'Utility Bill Payment',amount:-100},{id:4,description:'Online Purchase',amount:-30}]
+
+const totalBalance=transactions.reduce((acc,transaction)=>acc+transaction.amount,0)
+console.log('Total Balance: ',totalBalance)
+
+const maxiumTransaction = transactions.reduce((maximum, transaction) =>{
+    return transaction.amount > maximum ? transaction.amount:maximum}, transactions[0].amount)
+
+console.log('maximum Transaction: '+maxiumTransaction)
+const purchaseTransactions=transactions.filter(transaction=>transaction.amount<0)
+console.log(' Purchase Transactions: ',purchaseTransactions)
+const specificTransaction=transactions.find(transaction=>transaction.description==='Online Purchase')
+console.log('Specific Transaction: ',specificTransaction)
+const indexTransactionWithAmount=transactions.findIndex(transaction=>transaction.amount===-30)
+console.log('Index Transaction With Amount -30: ',indexTransactionWithAmount)
+transactions.forEach(transaction=>{if(transaction.amount<0){transaction.description=`Expense: ${transaction.description}`}else{transaction.description=`Income: ${transaction.description}`}})
+console.log('Udated Transactions: ',transactions)
