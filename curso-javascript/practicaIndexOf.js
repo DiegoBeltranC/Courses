b1a4127f7a7d23aa4d2cc3f9e0dd4f2d035d7766
sapomex <dabcpop@gmail.com>
2024-05-17 00:11:59 -0500

    added practicaIndex of.js

---
+++ curso-javascript/practicaIndexOf.js
@@ -0,0 +1,9 @@
+let arr = ['juan', 'pedro', 'rodolfo', 'maricarmen', 'juan']
+
+if (arr.includes('juan')){
+    console.log(arr.indexOf('juan'))
+    console.log(arr.lastIndexOf('juan'))
+
+}else{
+    console.log('It doesnt exist')
+}
