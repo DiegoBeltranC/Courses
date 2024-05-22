23291923c70c03267b9aba4bf473625f832a96ae
sapomex <dabcpop@gmail.com>
2024-05-21 21:01:30 -0500

    pequeños cambios casi curso js terminado, añadimos amend

--- proyect.js
+++ proyect.js
@@ -3,11 +3,11 @@ let teams = {"chivas": 0, "america": 0, "pumas": 0}
 let participants = [["chivas","america"],["america","pumas"],["pumas", "chivas"]]
 
 let winners = [0,0,1]
-
 participants.forEach((round, i) => {
     teams[round[winners[i]]] +=1})
 
-maximum = teams.get(teams[0].key())
+const key = Object.keys(teams)
+let maximum = key[0]
 
 for ([key,value] of teams.entries()){
     if(value > teams[maximum]){
